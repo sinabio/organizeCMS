bd2530563d5a8d57cd50b01145c2fce1a2c4902d
PrivateSniper <privatesniper@gmail.com>
2014-08-02 17:39:12 +0100

    Initial Git Push

--- app/views/install/configure.blade.php
+++ app/views/install/configure.blade.php
@@ -1,5 +1,32 @@
 @extends('layouts.master')
 
 @section('content')
-    
+    <h1>organizeCMS :: Installation</h1>
+    <div class="well bs-component col-lg-8">
+        <form class="form-horizontal">
+
+            <h2>Database</h2>
+
+            <div class="form-group">
+                <label for="host" class="control-label col-lg-2">host</label>
+                <input type="text" name="host" id="host" class="form-control col-lg-6" />
+            </div>
+
+            <div class="form-group">
+                <label for="database" class="control-label col-lg-2">database</label>
+                <input type="text" name="database" id="database" class="form-control col-lg-6" />
+            </div>
+
+            <div class="form-group">
+                <label for="db username" class="control-label col-lg-2">database username</label>
+                <input type="text" name="dbuser" id="dbuser" class="form-control col-lg-4" />
+            </div>
+
+
+        </form>
+    </div>
+@stop
+
+@section('sidebar')
+    <p>Sidebar</p>
 @stop
--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -2,16 +2,36 @@
 <html>
 
 <head>
+    <link rel="stylesheet" href="assets/css/bootstrap_slate.min.css" />
+    <link rel="stylesheet" href="assets/css/organizecms.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
-
+<body>
 <div class="container">
-    <header class="nav" role="navigation">
-        organizeCMS
-    </header>
+    <div class="navbar navbar-default navbar-fixed-top">
+        <header class="nav" role="navigation" id="navbar-main">
+            <a class="navbar-brand">organizeCMS</a>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="#">admin</a></li>
+                </ul>
+            </div>
+        </header>
+    </div>
 </div>
 
-<body>
-    @yield('content')
-</body>
+<div class="container" id="content">
 
+    <div class="col-xs-	8 col-sm-8 col-md-8 col-lg-10">
+        @yield('content')
+    </div>
+
+    <div class="col-xs-	4 col-sm-4 col-md-4 col-lg-2">
+        @yield('sidebar')
+    </div>
+</div>
+
+
+
+</body>
 </html>
